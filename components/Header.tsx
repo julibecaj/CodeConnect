0910ccfd91c5b740c78e3461c1128f51bba01fa6
julibecaj <julianbecaj@unyt.edu.al>
2025-10-31 17:52:15 +0100

    Refactor Header component and add responsive styles for navigation

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,19 +1,33 @@
 "use client";
 import Link from "next/link";
 import React from "react";
+import Image from "next/image";
 
 const Header: React.FC = () => {
   return (
-    <header className="cc-header">
-      <div className="cc-header__brand">
-        <div className="cc-logo">∞</div>
-        <span className="cc-brand">CodeConnect</span>
-      </div>
-      <nav className="cc-header__nav">
-        <Link className="cc-btn cc-btn--ghost" href="/login">Log In</Link>
-        <Link className="cc-btn cc-btn--solid" href="/signup">Sign Up</Link>
-      </nav>
-    </header>
+    // Outer gradient pill
+    <div className="cc-nav">
+      {/* Inner black bar */}
+      <header className="cc-track">
+        <div className="cc-header__brand">
+          <div className="cc-logo">
+            <Image
+              src="/assets/cc_logo2.svg"
+              alt="CodeConnect logo"
+              width={44}
+              height={44}
+              priority
+            />
+          </div>
+          <span className="cc-brand">CodeConnect</span>
+        </div>
+
+        <nav className="cc-header__nav">
+          <Link className="cc-btn cc-btn--ghost" href="/login">Log In</Link>
+          <Link className="cc-btn cc-btn--solid" href="/signup">Sign Up</Link>
+        </nav>
+      </header>
+    </div>
   );
 };
 

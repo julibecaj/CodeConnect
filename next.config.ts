cdb3f423e3d1e0b85ebc417651104841688c047d
julibecaj <julianbecaj@unyt.edu.al>
2026-01-05 02:59:58 +0100

    Add redirects configuration to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-
-
-  
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/CS",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
